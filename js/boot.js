e23cd8f6989f6cd4e83c9061d1752e28be12d890
IvonneBurgos <irburgos@espol.edu.ec>
2016-09-13 10:34:30 -0500

    no se

--- js/boot.js
+++ js/boot.js
@@ -5,10 +5,10 @@ var boot = function(game){
 boot.prototype = {
 	preload: function(){
         this.game.load.image('loading',"assets/images/GameImage-01.png");
-        this.game.load.sound('intro', ['assets/audio/Intro.mp3']);
-        this.game.load.sound('introlater', ['assets/audio/Intro.mp3']);
-        this.game.load.sound('win', ['assets/audio/win.mp3']);
-        this.game.load.sound('gameover', ['assets/audio/gameover.mp3']);
+        this.game.load.audio('intro', ['assets/audio/Intro.mp3']);
+        this.game.load.audio('introlater', ['assets/audio/Intro.mp3']);
+        this.game.load.audio('win', ['assets/audio/win.mp3']);
+        this.game.load.audio('gameover', ['assets/audio/gameover.mp3']);
         //this.game.load.sound('intro', ['assets/audio/Intro.mp3']);
          // this.game.add.sprite(800,800,'loading');
         console.log('hello its me');
